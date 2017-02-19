fdcfe0d2b839ecdb66123b549508ffe343efe0f3
Nicolás Hock Isaza <nhocki@gmail.com>
2017-02-18 23:14:19 -0500

    Adding single redis strategy test
    This commit adds a test for a single Redis strategy. This simulates a
    single server using redis strategy (which is pretty useless).
    
    All messages published will be received by the same redis host, which is
    forwarding every message to a single basic strategy.

---
+++ strategies/redis_test.go
@@ -0,0 +1,50 @@
+package strategies
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+
+	"github.com/nhocki/go-longpolling/models"
+	"github.com/nhocki/go-longpolling/redis"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSingleRedisStrategy(t *testing.T) {
+	strategy, err := NewRedisStrategy(redis.ConfigFromEnv())
+	assert.NoError(t, err)
+	strategy.Setup()
+
+	events := models.NewConnection()
+	messages := models.NewConnection()
+
+	assert.NoError(t, strategy.Add(events, "events"))
+	assert.NoError(t, strategy.Add(messages, "messages"))
+
+	assert.Equal(t, 1, strategy.TotalSubs("events"))
+	assert.Equal(t, 1, strategy.TotalSubs("messages"))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go assertEvent(t, events, wg)
+	go assertNoEvent(t, messages, wg)
+	strategy.Publish("events", bytes.NewReader([]byte(`Hello, World!`)))
+	wg.Wait()
+
+	wg.Add(2)
+	go assertEvent(t, messages, wg)
+	go assertNoEvent(t, events, wg)
+	strategy.Publish("messages", bytes.NewReader([]byte(`Hello, World!`)))
+	wg.Wait()
+
+	assert.Equal(t, 1, strategy.TotalSubs("events"))
+	assert.Equal(t, 1, strategy.TotalSubs("messages"))
+
+	assert.NoError(t, strategy.Remove(events.ID, "events"))
+	assert.NoError(t, strategy.Remove(messages.ID, "events"))
+	assert.NoError(t, strategy.Remove(messages.ID, "messages"))
+	assert.NoError(t, strategy.Remove(messages.ID, "messages"))
+
+	assert.Equal(t, 0, strategy.TotalSubs("events"))
+	assert.Equal(t, 0, strategy.TotalSubs("messages"))
+}
