2c18ae5c931cc9cfa1d4454cd0b3812f58697412
Nicolás Hock Isaza <nhocki@gmail.com>
2017-02-18 22:34:40 -0500

    Adding tests for the Basic strategy
    This commit adds tests for the `Basic` strategy. The test will add
    connections to multiple channels and test that publishing to those
    channels will send the message to the expected connections (and only
    those).

---
+++ strategies/basic_test.go
@@ -0,0 +1,49 @@
+package strategies
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+
+	"../models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicStrategy(t *testing.T) {
+	strategy := Basic{
+		log: models.NullLogger,
+	}
+
+	events := models.NewConnection()
+	messages := models.NewConnection()
+
+	assert.NoError(t, strategy.Add(events, "events"))
+	assert.NoError(t, strategy.Add(messages, "messages"))
+
+	assert.Equal(t, 1, strategy.TotalSubs("events"))
+	assert.Equal(t, 1, strategy.TotalSubs("messages"))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go assertEvent(t, events, wg)
+	go assertNoEvent(t, messages, wg)
+	strategy.Publish("events", bytes.NewReader([]byte(`Hello, World!`)))
+	wg.Wait()
+
+	wg.Add(2)
+	go assertEvent(t, messages, wg)
+	go assertNoEvent(t, events, wg)
+	strategy.Publish("messages", bytes.NewReader([]byte(`Hello, World!`)))
+	wg.Wait()
+
+	assert.Equal(t, 1, strategy.TotalSubs("events"))
+	assert.Equal(t, 1, strategy.TotalSubs("messages"))
+
+	assert.NoError(t, strategy.Remove(events.ID, "events"))
+	assert.NoError(t, strategy.Remove(messages.ID, "events"))
+	assert.NoError(t, strategy.Remove(messages.ID, "messages"))
+	assert.NoError(t, strategy.Remove(messages.ID, "messages"))
+
+	assert.Equal(t, 0, strategy.TotalSubs("events"))
+	assert.Equal(t, 0, strategy.TotalSubs("messages"))
+}
---
+++ strategies/strategies_test.go
@@ -0,0 +1,31 @@
+package strategies
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"../models"
+)
+
+func assertEvent(t *testing.T, c *models.Connection, wg *sync.WaitGroup) {
+	select {
+	case <-c.C:
+		// no-op
+	case <-time.After(1 * time.Second):
+		t.Errorf("Did not get event")
+		t.Fail()
+	}
+	wg.Done()
+}
+
+func assertNoEvent(t *testing.T, c *models.Connection, wg *sync.WaitGroup) {
+	select {
+	case <-c.C:
+		t.Errorf("Got event when none was expected")
+		t.Fail()
+	case <-time.After(300 * time.Millisecond):
+		// no-op
+	}
+	wg.Done()
+}
